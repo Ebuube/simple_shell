b83a78293607dcebb735dde4be1d36014b98b1cd
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-13 09:39:07 +0000

    Implemented the use of command lines in cmd
    Removed tests.
    Codebase ready for submission.

--- sh_execute.c
+++ sh_execute.c
@@ -10,7 +10,6 @@ pid_t sh_execute(char *cmd)
 {
 	char **args = NULL;
 	pid_t proc_id = 0, status = 0;
-	int i = 0;	/* test */
 
 	args = tokenize(cmd);
 	if (cmd == NULL || args == NULL)
@@ -20,18 +19,13 @@ pid_t sh_execute(char *cmd)
 	if (args[0] == NULL)
 	{/* no program to run */
 		free(args);
-		args = NULL;
 		return (0);
 	}
-	for (i = 0; args[i]; i++)
-		printf("args[%d] -> '%s'\n", i, args[i]);	/* test */
-
 	proc_id = fork();
 	if (proc_id == -1)
 	{/* Error forking this process */
 		return (-1);
 	}
-
 	if (proc_id == 0)
 	{/* Child process */
 		status = execve(args[0], args, environ);
@@ -42,21 +36,15 @@ pid_t sh_execute(char *cmd)
 		}
 		if (cmd)
 			free_str_safe(&cmd);
-
 		if (status < 0)
 		{/* Error executing command */
 			perror(ERR_PROMPT);
 			exit(EXIT_FAILURE);
 		}
 	}
-
-	if (proc_id > 0)
-	{/* Parent process */
+	if (proc_id > 0)	/* Parent process */
 		wait(NULL);
-	}
-
 	if (args)
 		free(args);
-
 	return (status);
 }
